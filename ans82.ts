0a9534d5584696a10e8e073fa0d500b0d00fb21e
asadullah48 <asadullahshafique@hotmail.com>
2024-05-25 15:47:53 +0500

    Initial commit

---
+++ ans82.ts
@@ -0,0 +1,9 @@
+// Q 82
+// This function counts how many characters are in a string
+function stringLength(str: string): number {
+    return str.length; // Returns the number of characters in the string
+  }
+  
+  // Example: Measuring the length of a name
+  console.log(stringLength("Asadullah Shafique")); // Outputs: 18
+  // We're simply asking how long the string "Alice" is, and it tells us there are 5 characters.
